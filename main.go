4f351c006f754aa4182f669e038ec0f97fea4774
João Sallé <jhonnyot@gmail.com>
2019-11-20 22:36:53 -0300

    alteracoes

--- main.go
+++ main.go
@@ -13,10 +13,10 @@ import (
 
 const url = "http://localhost:8080"
 
-func postRequest() error {
+func postRequest(dificuldade int) error {
 	rand.Seed(time.Now().UnixNano())
 	dados := rand.Intn(1e6)
-	reqJSON := []byte(`{"Dados":` + strconv.Itoa(dados) + `}`)
+	reqJSON := []byte(`{"Dados":` + strconv.Itoa(dados) + `, "Dificuldade":` + strconv.Itoa(dificuldade) + `}`)
 	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(reqJSON))
 	req.Header.Set("Content-Type", "application/json")
 	client := &http.Client{Timeout: 0 * time.Second}
@@ -44,7 +44,7 @@ func main() {
 		milis := rand.Intn(99)
 		secs := rand.Intn(60)
 		t := (time.Duration(secs) * time.Second) + (time.Duration(milis) * time.Millisecond)
-		go postRequest()
+		// go postRequest()
 		time.Sleep(t)
 	}
 

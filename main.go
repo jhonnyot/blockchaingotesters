7b6540e6ca5c45768bafb54c37d27b649a4f4cd1
Sallé <jhonnyot@gmail.com>
2021-12-01 19:18:17 -0300

    Correcao teste 4

--- main.go
+++ main.go
@@ -193,9 +193,7 @@ func (cart *Carteira) start(ctx context.Context, cancel *context.CancelFunc) {
 	if len(transactions) > 15 {
 		if true || rand.Intn(100) >= 50 {
 			if _, ok := cartMaliciosas[cart.ID.String()]; !ok || !malicious {
-				mutexBC.Lock()
 				novoBloco := geraBloco(ctx, blockchain[len(blockchain)-1], transactions, dificuldade, cart.ID.String(), false)
-				mutexBC.Unlock()
 				if insertBloco(novoBloco) {
 					if verbose >= 1 {
 						spew.Dump("Bloco inserido com sucesso. " + time.Now().Format("15:04:05"))
@@ -205,9 +203,7 @@ func (cart *Carteira) start(ctx context.Context, cancel *context.CancelFunc) {
 				}
 			} else if ok {
 				if txs := getMaliciousTXs(); !cmp.Equal(txs, []Transacao{}) {
-					mutexBC.Lock()
 					novoBloco := geraBloco(ctx, blockchain[len(blockchain)-1], txs, dificuldade, cart.ID.String(), true)
-					mutexBC.Unlock()
 					if insertBloco(novoBloco) {
 						if verbose >= 1 {
 							spew.Dump("Bloco malicioso inserido com sucesso. " + time.Now().Format("15:04:05"))
@@ -225,8 +221,8 @@ func (cart *Carteira) start(ctx context.Context, cancel *context.CancelFunc) {
 }
 
 func insertBloco(novoBloco Bloco) bool {
+	mutexBC.Lock()
 	if (!cmp.Equal(novoBloco, Bloco{})) && blocoValido(novoBloco, blockchain[len(blockchain)-1]) {
-		mutexBC.Lock()
 		blockchain = append(blockchain, novoBloco)
 		if len(blockchain)%100 == 0 {
 			salvaEstado()
@@ -237,6 +233,7 @@ func insertBloco(novoBloco Bloco) bool {
 		mutexBC.Unlock()
 		return true
 	}
+	mutexBC.Unlock()
 	return false
 }
 

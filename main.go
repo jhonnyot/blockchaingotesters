763b661c627e437dc47492b73a3a65634972ac7b
Sallé <jhonnyot@gmail.com>
2021-12-01 19:30:20 -0300

    Correcao teste 10

--- main.go
+++ main.go
@@ -198,7 +198,6 @@ func (cart *Carteira) start(ctx context.Context, cancel *context.CancelFunc) {
 					if verbose >= 1 {
 						spew.Dump("Bloco inserido com sucesso. " + time.Now().Format("15:04:05"))
 					}
-					mutexBC.Unlock()
 					c := *cancel
 					c()
 				}
@@ -209,7 +208,6 @@ func (cart *Carteira) start(ctx context.Context, cancel *context.CancelFunc) {
 						if verbose >= 1 {
 							spew.Dump("Bloco malicioso inserido com sucesso. " + time.Now().Format("15:04:05"))
 						}
-						mutexBC.Unlock()
 						c := *cancel
 						c()
 					}
@@ -231,8 +229,10 @@ func insertBloco(novoBloco Bloco) bool {
 		mutexTrans.Lock()
 		limpaTransacoes()
 		mutexTrans.Unlock()
+		mutexBC.Unlock()
 		return true
 	}
+	mutexBC.Unlock()
 	return false
 }
 

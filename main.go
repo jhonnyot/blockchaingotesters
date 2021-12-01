4ea60da1734ca99973a085197da0effe3a6da077
Sallé <jhonnyot@gmail.com>
2021-12-01 19:20:50 -0300

    Correcao teste 5

--- main.go
+++ main.go
@@ -193,7 +193,9 @@ func (cart *Carteira) start(ctx context.Context, cancel *context.CancelFunc) {
 	if len(transactions) > 15 {
 		if true || rand.Intn(100) >= 50 {
 			if _, ok := cartMaliciosas[cart.ID.String()]; !ok || !malicious {
+				mutexBC.Lock()
 				novoBloco := geraBloco(ctx, blockchain[len(blockchain)-1], transactions, dificuldade, cart.ID.String(), false)
+				mutexBC.Unlock()
 				if insertBloco(novoBloco) {
 					if verbose >= 1 {
 						spew.Dump("Bloco inserido com sucesso. " + time.Now().Format("15:04:05"))
@@ -203,7 +205,9 @@ func (cart *Carteira) start(ctx context.Context, cancel *context.CancelFunc) {
 				}
 			} else if ok {
 				if txs := getMaliciousTXs(); !cmp.Equal(txs, []Transacao{}) {
+					mutexBC.Lock()
 					novoBloco := geraBloco(ctx, blockchain[len(blockchain)-1], txs, dificuldade, cart.ID.String(), true)
+					mutexBC.Unlock()
 					if insertBloco(novoBloco) {
 						if verbose >= 1 {
 							spew.Dump("Bloco malicioso inserido com sucesso. " + time.Now().Format("15:04:05"))
@@ -230,7 +234,6 @@ func insertBloco(novoBloco Bloco) bool {
 		mutexTrans.Lock()
 		limpaTransacoes()
 		mutexTrans.Unlock()
-		mutexBC.Unlock()
 		return true
 	}
 	mutexBC.Unlock()
